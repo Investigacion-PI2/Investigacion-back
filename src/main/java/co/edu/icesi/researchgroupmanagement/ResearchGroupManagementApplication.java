93ec0bfdcc06f86bc772c89a33e31fcd90a3eaf5
gevorah <jhonsebastianijajiortiz@gmail.com>
2022-10-13 19:46:27 -0500

    base

---
+++ src/main/java/co/edu/icesi/researchgroupmanagement/ResearchGroupManagementApplication.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.researchgroupmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResearchGroupManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ResearchGroupManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/edu/icesi/researchgroupmanagement/ResearchGroupManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package co.edu.icesi.researchgroupmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResearchGroupManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
