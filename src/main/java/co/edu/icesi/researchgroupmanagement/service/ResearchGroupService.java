07447b555b0b17773f2fff7baac9b018d30222a2
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2022-10-17 20:26:39 -0500

    added some more validations and added the  research group rest controller

--- src/main/java/co/edu/icesi/researchgroupmanagement/service/ResearchGroupService.java
+++ src/main/java/co/edu/icesi/researchgroupmanagement/service/ResearchGroupService.java
@@ -1,23 +1,37 @@
 package co.edu.icesi.researchgroupmanagement.service;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import co.edu.icesi.researchgroupmanagement.model.Faculty;
 import co.edu.icesi.researchgroupmanagement.model.ResearchGroup;
+import co.edu.icesi.researchgroupmanagement.repository.FacultyRepository;
+import co.edu.icesi.researchgroupmanagement.repository.InstitutionRepository;
 import co.edu.icesi.researchgroupmanagement.repository.ResearchGroupRepository;
+import co.edu.icesi.researchgroupmanagement.repository.SciTiNationalProgramRepository;
 
 @Service
 public class ResearchGroupService {
     ResearchGroupRepository rgr;
+    FacultyRepository fr;
+    InstitutionRepository ir;
+    SciTiNationalProgramRepository scitir;
 
     @Autowired
-    public ResearchGroupService(ResearchGroupRepository rgr) {
+    public ResearchGroupService(ResearchGroupRepository rgr, FacultyRepository fr,
+            SciTiNationalProgramRepository scitir, InstitutionRepository ir) {
         this.rgr = rgr;
+        this.fr = fr;
+        this.ir = ir;
+        this.scitir = scitir;
     }
 
     @Transactional
     public ResearchGroup save(ResearchGroup rg) {
+
         if (rg == null) {
             throw new IllegalArgumentException("El grupo de investigacion no puede ser nulo");
         } else if (rg.getName() == null) {
@@ -40,6 +54,12 @@ public class ResearchGroupService {
             throw new IllegalArgumentException("Intitucion nula");
         } else if (rg.getSciTiNationalProgram() == null) {
             throw new IllegalArgumentException("SciTiNatProgram nulo");
+        } else if (fr.findById(rg.getFaculty().getId()).isEmpty()) {
+            throw new IllegalArgumentException("Facultad no existe");
+        } else if (ir.findById(rg.getInstitution().getId()).isEmpty()) {
+            throw new IllegalArgumentException("Institucion no existe");
+        } else if (scitir.findById(rg.getSciTiNationalProgram().getId()).isEmpty()) {
+            throw new IllegalArgumentException("SciTiNationalProgram no existe");
         } else {
             rgr.save(rg);
             return rg;
