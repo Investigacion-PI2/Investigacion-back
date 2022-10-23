be26757976af081f3b83c7de811004ce290c3a98
Sebastian Jaramillo <sebastian.jaramillo2@correo.icesi.edu.co>
2022-10-23 13:58:59 -0500

    Added some validations to add research group and added a method to get the research groups by id and get it all

--- src/main/java/co/edu/icesi/researchgroupmanagement/controller/ResearchGroupController.java
+++ src/main/java/co/edu/icesi/researchgroupmanagement/controller/ResearchGroupController.java
@@ -1,6 +1,8 @@
 package co.edu.icesi.researchgroupmanagement.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -19,5 +21,15 @@ public class ResearchGroupController {
     @PostMapping
     public void save(@RequestBody ResearchGroup rg) {
         service.save(rg);
+    }
+
+    @GetMapping
+    public Iterable<ResearchGroup> findAll() {
+        return service.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResearchGroup findById(@PathVariable("id") Long id) {
+        return service.findById(id);
     }
 }
